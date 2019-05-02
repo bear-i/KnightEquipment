3fc44ef399ffbe586baf97736bbd29a4c16f3d54
bear_i <rnmdvd@gmail.com>
2019-05-02 18:48:21 +0300

    Removed useless variable

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -1,12 +1,10 @@
 package com.epam;
 
 import com.epam.service.AppServiceImpl;
-import com.epam.service.MenuServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         AppServiceImpl app = new AppServiceImpl();
-        MenuServiceImpl menu = new MenuServiceImpl();
         app.setKnightBalance();
         app.chooseOptionFromStartingMenu();
         System.out.println("Bye-bye!");
